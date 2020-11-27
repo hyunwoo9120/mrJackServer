6a1cafcb5c88107cce70d0b3732ce2b636b6726d
hyunwoo9120 <hyunwoo.k9120@gmail.com>
2020-11-27 20:31:36 +0900

    GameData

--- GameData.cpp
+++ GameData.cpp
@@ -8,13 +8,16 @@ GameData::~GameData()
 {
 }
 
+
+/// <summary>
+/// 홀수 라운드 시작할 때, 8개의 카드 중 랜덤으로 4개를 뽑는 함수
+/// </summary>
 void GameData::randomCard() {
 	srand((unsigned int)time(NULL));
 	
 	for (int i = 0; i < 8; i++) {					// 홀수 라운드마다 모두 0으로 초기화
 		this->selectedCard[i] = 0;
 	}
-
 	while (1) {
 		int n = rand() % 8;
 		this->selectedCard[n] = 1;				// 뽑힌 카드는 1로 표시
@@ -29,6 +32,11 @@ void GameData::randomCard() {
 	}
 }
 
+
+/// <summary>
+/// 홀수 라운드에서 짝수 라운드로 바뀔 때,
+/// 전 라운드에서 사용하지 않은 카드를 세팅해주는 함수
+/// </summary>
 void GameData::remainCard() {
 	for (int i = 0; i < 8; i++) {
 		if (this->selectedCard[i] == 1) {
@@ -41,8 +49,10 @@ void GameData::remainCard() {
 	// 홀수일때 1인 카드를 0으로, 0인 카드를 1로 바꿈!
 }
 
-
-
+/// <summary>
+/// 
+/// </summary>
+/// <returns></returns>
 int* GameData::InfoGameRound() {
 
 	if (turn < 4) {
@@ -55,6 +65,10 @@ int* GameData::InfoGameRound() {
 }
 
 
+/// <summary>
+/// 유저의 순서를 확인하는 함수
+/// </summary>
+/// <returns>0이면 잭의 차례, 1이면 수사관의 차례</returns>
 int GameData::check_user() {
 	int oddround[4] = { 1, 0, 0, 1 };				// 홀수 : 수사관(1)부터
 	int evenround[4] = { 0, 1, 1, 0 };				// 짝수 : 잭(0)부터
@@ -68,13 +82,39 @@ int GameData::check_user() {
 	// return 값이 1이면 수사관 차례, 0이면 잭 차례
 }
 
-bool GameData::update_Jack() {
 
-	if (/*잭이 밝은 곳에 존재함*/) {					// 가로등 범위, 왓슨 손전등 범위, 주위 사람 유무 확인
-		JackState = true;
+/// <summary>
+/// 잭이 빛에 있는지, 어둠에 있는지 확인해서 JackState를 세팅하는 함수
+/// </summary>
+/// <returns>없음</returns>
+void GameData::update_Jack() {
+	if (light_character(/*Jack 캐릭터의 열거형 변수*/))
+	{					
+		this->JackState= true;
 	}
 	else {
-		JackState = false;
+		this->JackState = false;
 	}
-	return JackState;
 }
+
+
+/// <summary>
+/// 각 캐릭터가 빛에 있는지 확인하는 함수
+/// 인자로 캐릭터 변수(열거형)를 넣는다.
+/// </summary>
+/// <returns>밝은 곳에 있으면 true, 어두운 곳에 있으면 false</returns>
+bool GameData::light_character(int character) {
+	// TODO: 가로등 범위, 왓슨 손전등 범위, 주위 사람 유무 확인
+	if (/*가로등 옆 || 주위 캐릭터 옆 || 왓슨 손전등 범위 안*/) { return true; }
+	else return false;
+}
+
+
+/// <summary>
+/// 8개의 카드 중 잭 역할의 캐릭터를 고르고 세팅하는 함수
+/// </summary>
+void GameData::set_Jack() {
+	srand((unsigned int)time(NULL));
+	this->jack = (Characters)(rand() % 8);
+}
+
--- GameData.h
+++ GameData.h
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <ctime>
 #pragma once
+enum Characters:int{
+	Holmes = 0,
+	Watson,
+	Smith,
+	Lestrade,
+	Stealthy,
+	Goodley,
+	William,
+	Jeremy
+};
 class GameData
 {
 public:
@@ -8,20 +18,20 @@ public:
 	int selectedCard[8] = { 0, 0};				// 뽑힌 카드는 1로 표현
 	int RoundTurn[2] = { 1,0 };				// round, turn 알려주는 배열( [0] : round, [1] : turn )
 
-	int selectedCard[8] = { 0,0 };				// 선택된 카드(1) 알려줌. 
+	int usedCard[8] = { 0,0 };				// 게임에서 사용된 카드: 선택된 카드(1) 알려줌. 
 	bool JackState = true;					// 잭의 상태,  우선 '빛에 있다' 가정하고 시작
-
+	Characters jack;
 
 	GameData();
 	~GameData();
 
 	void randomCard();					// 홀수라운드마다 캐릭터 카드 4장 뽑는 함수 
 	void remainCard();					//	짝수라운드일 때 나머지 카드 4장 뽑는 함수
-
+	void set_Jack();					// 게임 시작 시 Jack을 랜덤으로 뽑아야한다
 	int* InfoGameRound();					// turn 시작마다 round, turn 알려주는 함수(전체 게임 시작시에도 실행)
 	int check_user();					// 잭 차례인지 수사관 차례인지 알려주는 함수
-	bool update_Jack();					// 잭이 빛에 있는지, 어둠에 있는지 알려주는 함수
-
+	void update_Jack();					// 잭이 빛에 있는지, 어둠에 있는지 확인해서 JackState를 세팅하는 함수
+	bool light_character(int);
 
 	/*	
 	추가로 구현해야 할 것
