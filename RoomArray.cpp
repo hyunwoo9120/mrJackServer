0225ccf74204581a194ae09e13d710c05c946342
hyunwoo9120 <hyunwoo.k9120@gmail.com>
2020-12-08 19:55:29 +0900

    recv and send data as file

--- RoomArray.cpp
+++ RoomArray.cpp
@@ -10,49 +10,88 @@ void RoomArray::print() {
 	cout << "\n";
 }
 
+void RoomArray::saveData()
+{
+	recvData.open("../data.txt", ios::out);
+	recvData.write((char*)&this->gameData, sizeof(this->gameData));
+	recvData.close();
+}
+
+
 int RoomArray::sendUsers() {
-	//fgets(cMsg, PACKET_SIZE, stdin);
-	char cMsg[PACKET_SIZE] = "Game Start!";
-	printf("데이터를 모든 유저에게 보냅니다: %s\n", cMsg);
-	int result = send(user[0], cMsg, strlen(cMsg), 0);
-	if (result < 0)
-		return -1;
 
-	result = send(user[1], cMsg, strlen(cMsg), 0);
-	if (result < 0)
-		return -1;
+	// 데이터를 성공적으로 받았는데 홀수 라운드에 첫번째 턴일 경우 랜덤으로 4장 뽑기
+	if (this->gameData.RoundTurn[1] == 1 && this->gameData.RoundTurn[0] % 2 != 0)
+		this->gameData.randomCard();
 
+	// 짝수 라운드에 첫번재 턴일 경우 남은 카드 4장 사용하기
+	else if (this->gameData.RoundTurn[1] == 1 && this->gameData.RoundTurn[0] % 2 == 0)
+		this->gameData.remainCard();
+
+	// 일단 객체를 파일로 다시 저장하는 작업이 필요함
+	saveData();
+
+	// 파일 열기
+	sendData.open("../data.txt", ios::in);
+
+	// 파일 크기만큼을 dataBuffer에 저장
+	sendData.read(dataBuffer, STRUCT_SIZE);
+	sendData.close();
+
+	// data의 내용을 유저에게 보냄
+	printf("데이터를 모든 유저에게 보냅니다\n");
+	int result = send(user[0], dataBuffer, sizeof(dataBuffer), 0);
+	if (result < 0)
+	{
+		return -1;
+	}
+	result = send(user[1], dataBuffer, sizeof(dataBuffer), 0);
+	if (result < 0)
+	{
+		return -1;
+	}
 	return 1;
 }
 
 int RoomArray::recvUser1() {
-	int recvResult = recv(user[0], cBuffer, PACKET_SIZE, 0);
-	//strcpy_s(cMsg, cBuffer);
-	memcpy(cMsg, cBuffer, PACKET_SIZE);
-	for (int i = 0; i < PACKET_SIZE; i++) {
-		cBuffer[i] = '\0';
-	}
+	int recvResult;
+
+	recvResult = recv(user[0], dataBuffer, sizeof(dataBuffer), 0);
+	if (recvResult == -1) return recvResult;
+
+	recvData.open("../data.txt", ios::out);
+	recvData.write(dataBuffer, recvResult);
+	recvData.close();
+
+	// 받은 데이터를 내 게임 데이터로 이식
+	sendData.open("../data.txt", ios::in);
+	sendData.seekg(0);
+	sendData.read((char*)&this->gameData, sizeof(this->gameData));
+	sendData.close();
+
 	return recvResult;
 }
+
 int RoomArray::recvUser2() {
-	int recvResult = recv(user[1], cBuffer, PACKET_SIZE, 0);
-	//strcpy_s(cMsg, cBuffer);
-	memcpy(cMsg, cBuffer, PACKET_SIZE);
-	for (int i = 0; i < PACKET_SIZE; i++) {
-		cBuffer[i] = '\0';
-	}
+	int recvResult;
+
+	recvResult = recv(user[1], dataBuffer, sizeof(dataBuffer), 0);
+	if (recvResult == -1) return recvResult;
+
+	recvData.open("../data.txt", ios::out);
+	recvData.write(dataBuffer, recvResult);
+	recvData.close();
+
+	// 받은 데이터를 보낼 데이터로 가공하기 위해 내 게임 데이터로 이식
+	sendData.open("../data.txt", ios::in);
+	sendData.seekg(0);
+	sendData.read((char*)&this->gameData, sizeof(this->gameData));
+	sendData.close();
+
 	return recvResult;
 }
 
 void RoomArray::GameStart() {
-	// TODO
-	// Round turn에 따라서
-	// 홀수 라운드: 수사관 잭 잭 수사관
-	// 짝수 라운드: 잭 수사관 수사관 잭
-
-	//GameData gameData;
-
-	// 처음에 유저0에게는 잭을, 유저1에게는 수사관임을 알려줘야함
 	printf("유저에게 역할을 보냅니다.\n");
 	int result = send(user[0], "Jack", strlen("Jack"), 0);
 	printf("Jack을 결정하였습니다.\n");
@@ -64,65 +103,64 @@ void RoomArray::GameStart() {
 	if (result < 0)
 		printf("error: send to Police");
 
-	// 초기 게임 데이터를 유저에게 전달
 	// 게임에 필요한 데이터를 주고 받는다
-	//while (1) { 
-	printf("\n============================\n");
+	printf("\n=========================================\n");
+	printf("(시작) 두 유저에게 역할을 전달했습니다.\n");
+
+	printf("8 캐릭터 카드 중 잭 역할의 캐릭터를 선정했습니다..\n");
+	this->gameData.set_Jack();
+
+	// 유저에게 초기 데이터를 보냄
 	int sendResult = sendUsers();
 	if (sendResult == -1) {
 		printf("Send Err\n\n");
 		closesocket(user[0]);
 		closesocket(user[1]);
-		//break;
 	}
-	printf("(시작)두 유저에게 데이터를 보냈습니다.\n");
-	/*while (1) {
-		int recvResult = recvUser1();
-		printf("\n유저1에게 데이터를 받았습니다: %s\n", cMsg);
-		if (recvResult < 0) {
-			printf("Recv Err\n\n");
-			break;
-		}
-		else
-		{
-			int sendResult = sendUsers();
-			if (sendResult == -1) {
-				printf("Send Err\n\n");
-				closesocket(user[0]);
-				closesocket(user[1]);
+
+	while(this->gameData.whoWin == -1){
+
+		if (this->gameData.check_user() == 0) {
+			//잭 유저의 차례
+			int recvResult = recvUser1();
+			printf("\n\'잭\' 유저에게 데이터를 받았습니다\n");
+			if (recvResult < 0) {
+				printf("Recv Err\n\n");
 				break;
 			}
-		}
-
-		recvResult = recvUser2();
-		printf("\n유저2에게 데이터를 받았습니다: %s\n", cMsg);
-		if (recvResult < 0) {
-			printf("Recv Err\n\n");
-			break;
-		}
-		else
-		{
-			int sendResult = sendUsers();
-			if (sendResult == -1) {
-				printf("Send Err\n\n");
-				closesocket(user[0]);
-				closesocket(user[1]);
-				break;
+			else
+			{
+				int sendResult = sendUsers();
+				if (sendResult == -1) {
+					printf("Send Err\n\n");
+					closesocket(user[0]);
+					closesocket(user[1]);
+					break;
+				}
 			}
 		}
+		if (this->gameData.check_user() == 1) {
+			//수사관 유저의 차례
+			int recvResult = recvUser2();
+			printf("\n\'수사관\' 유저에게 데이터를 받았습니다\n");
+			if (recvResult < 0) {
+				printf("Recv Err\n\n");
+				break;
+			}
+			else
+			{
+				int sendResult = sendUsers();
+				if (sendResult == -1) {
+					printf("Send Err\n\n");
+					closesocket(user[0]);
+					closesocket(user[1]);
+					break;
+				}
+			}
+		}
+	}
 
-		//	for (int i = 0; i < PACKET_SIZE; i++) {
-		//		cBuffer[i] = '\0';
-		//	}
-	}break;
-}
-
-printf("\n===== Client Disconnected =====\n");
-closesocket(user[0]);
-closesocket(user[1]);*/
-}
-
-void RoomArray::initGameDate()
-{
-	this->gameData;
+	printf("\n========== Client Disconnected ==========\n");
+	closesocket(user[0]);
+	closesocket(user[1]);
 }
--- RoomArray.h
+++ RoomArray.h
@@ -2,9 +2,11 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <string.h>
+#include <fstream>
+#include <cstdlib>
 #include "GameData.h"
 #pragma comment(lib, "ws2_32")
-#define PACKET_SIZE 1024
+#define STRUCT_SIZE 2720
 
 #pragma once
 #define ROOMCNT 5
@@ -12,22 +14,24 @@ using namespace std;
 
 class RoomArray {
 public:
-	char cBuffer[PACKET_SIZE] = {}; // 게임 데이터
-	char cMsg[PACKET_SIZE] = "GameStart"; // 가공된 데이터(유저한테 보낼 것)
+	ofstream recvData;
+	ifstream sendData;
+	GameData gameData;
+
+	char dataBuffer[STRUCT_SIZE] = ""; // 게임 데이터 받을 버퍼
 
 	int userCnt=0; 
 	// user[0]: 잭, user[1]: 수사관
 	SOCKET user[2];
-	GameData gameData;
 
 	RoomArray();
 	~RoomArray();
 
 	void print();
+	void saveData();
 	int sendUsers();
 	int recvUser1();
 	int recvUser2();
 
 	void GameStart();
-	void initGameDate();
 };
